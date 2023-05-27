54526ab80a674e81c511ccd08de7200f7ed1e6e7
iLVOEWOCK <131495628+iLVOEWOCK@users.noreply.github.com>
2023-05-27 10:11:22 -0700

    Ui Implementation StatsCommand.php

--- src/wockgod/command/StatsCommand.php
+++ src/wockgod/command/StatsCommand.php
@@ -2,6 +2,7 @@
 
 namespace wockgod\command;
 
+use jojoe77777\FormAPI\SimpleForm;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\player\Player;
@@ -18,20 +19,50 @@ class StatsCommand extends Command {
             $sender->sendMessage("§cUsage: /stats <player>");
             return;
         }
+
         $playerName = $args[0];
         $playerStats = KDR::getInstance()->getConfig()->getNested("players.$playerName");
+
         if ($playerStats === null) {
             $sender->sendMessage("§cPlayer not found.");
             return;
         }
+
         $kills = $playerStats["kills"];
         $deaths = $playerStats["deaths"];
         $kdr = $deaths > 0 ? round($kills / $deaths, 2) : $kills;
-        $message = KDR::getInstance()->getConfig()->getNested("messages.stats", "{player}'s Statistics: KDR: {kdr} (Kills: {kills}, Deaths: {deaths}).");
-        $message = str_replace("{player}", $playerName, $message);
-        $message = str_replace("{kdr}", $kdr, $message);
-        $message = str_replace("{kills}", $kills, $message);
-        $message = str_replace("{deaths}", $deaths, $message);
-        $sender->sendMessage($message);
+
+        if (KDR::getInstance()->getConfig()->get("form-ui")) {
+            $form = new SimpleForm(function (Player $player, ?int $data) use ($playerName, $kills, $deaths, $kdr) {
+                if ($data === null) {
+                    return;
+                }
+
+                $message = KDR::getInstance()->getConfig()->getNested("messages.stats", "{player}'s Statistics: KDR: {kdr} (Kills: {kills}, Deaths: {deaths}).");
+                $message = str_replace("{player}", $playerName, $message);
+                $message = str_replace("{kdr}", $kdr, $message);
+                $message = str_replace("{kills}", $kills, $message);
+                $message = str_replace("{deaths}", $deaths, $message);
+                $player->sendMessage($message);
+            });
+
+            $form->setTitle(KDR::getInstance()->getConfig()->getNested("ui.stats.title", "Stats"));
+            $form->setContent(KDR::getInstance()->getConfig()->getNested("ui.stats.content", "{player}'s Statistics:\nKDR: {kdr}\nKills: {kills}\nDeaths: {deaths}"));
+            $form->addButton(KDR::getInstance()->getConfig()->getNested("ui.stats.close-button", "Close"));
+
+            $form->setContent(str_replace("{player}", $playerName, $form->getContent()));
+            $form->setContent(str_replace("{kdr}", $kdr, $form->getContent()));
+            $form->setContent(str_replace("{kills}", $kills, $form->getContent()));
+            $form->setContent(str_replace("{deaths}", $deaths, $form->getContent()));
+
+            $sender->sendForm($form);
+        } else {
+            $message = KDR::getInstance()->getConfig()->getNested("messages.stats", "{player}'s Statistics: KDR: {kdr} (Kills: {kills}, Deaths: {deaths}).");
+            $message = str_replace("{player}", $playerName, $message);
+            $message = str_replace("{kdr}", $kdr, $message);
+            $message = str_replace("{kills}", $kills, $message);
+            $message = str_replace("{deaths}", $deaths, $message);
+            $sender->sendMessage($message);
+        }
     }
 }
